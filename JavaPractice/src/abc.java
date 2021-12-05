125394e36438f62cf310f7b79836daca0e4eddb7
Bibrat <happy.justu143@gmail.com>
2021-12-05 13:43:09 +0530

    deployed from java111

--- JavaPractice/src/abc.java
+++ JavaPractice/src/abc.java
@@ -4,9 +4,11 @@ public class abc {
 	public static void main(String[] args) {
 		int i=5;
 		int b=2;
-		int c=i/b
-;		System.out.println(c);
-
+		int c=i/b;		
+		
+		System.out.println(c);
+		System.out.println(" Java practice 111");
+		System.out.println("Java practice 111");
 	}
 
 }
